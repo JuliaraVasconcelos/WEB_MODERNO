36412e02f1ccff63b7965255356b4a4f6c4dab4c
Juliara <juliara_vasconcelos@hotmail.com>
2022-06-02 11:49:08 -0300

    att2

--- fundamentos/hoisting.js
+++ fundamentos/hoisting.js
@@ -1,4 +1,17 @@
-console.log('a=', a)
+console.log('a =', a);
 var a = 2
-console.log('a =', a)
-var a 
+console.log('a =', a);
+
+function teste(){
+    console.log('a =', a);
+    var a = 2
+    console.log('a =', a);
+}
+
+teste();
+
+//os dois tem o mesmo efeito
+
+console.log('b=', b)
+let b = 2
+console.log('b=', b);
